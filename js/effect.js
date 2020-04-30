b8a70b38ce305d88aa52d61a0e2e980e8b833d2e
airititim <airititim0816@gmail.com>
2020-04-30 17:45:56 +0800

    test

--- js/effect.js
+++ js/effect.js
@@ -1,35 +1,44 @@
 $(document).ready(function () {
-    var ContentWidth = $('.book-fair-area').width();
-    var Size = 2;
-    $('.book-fair-area').css('height', ContentWidth * Size);
-    $(window).resize(function () {
-        var ContentWidth = $('.book-fair-area').width();
-        var Size = 2;
-        $('.book-fair-area').css('height', ContentWidth * Size);
-    });
-    /*$(window).scroll(function () { 
-        var ScrollTop = $(window).scrollTop();
-        var BooksPos = $('.books-list').offset().top;
-        console.log(ScrollTop + '&' + BooksPos);
-        if ( ScrollTop*1.25 >= BooksPos ) {
-        } else {
-        }
-    });*/
-    $('.books-fair-btn').click(function (e) { 
-        $('.books-list-btn-inside').slideToggle();
+    var BookFairNmae = $('.books-list-btn-show span').html();
+    $('.book-fair-title').html(BookFairNmae);
+    /*展開全部書展*/
+    $('.books-fair-btn-more').click(function (e) {
+        $('.books-fair-all').stop().slideToggle(1000);
+        $(this).toggleClass('books-fair-all-active');
         return false;
     });
 
+    /*書展頁籤功能*/
+    $('.books-list-btn-show').click(function (e) {
+        $('.book-fair-list').fadeOut(0);
+        $($(this).attr('href')).fadeIn(0);
+        return false;
+    });
+    $('.books-fair-btn li a').not('.books-fair-btn-more').click(function (e) {
+        var BookFairNmae = $(this).children('span').html();
+        $('.books-fair-btn li a').removeClass('books-fair-btn-active');
+        $('.books-fair-all li a').removeClass('books-fair-list-active');
+        $(this).addClass('books-fair-btn-active');
+        $('.book-fair-title').html(BookFairNmae);
+    });
+    $('.books-fair-all li a').click(function (e) {
+        var BookFairNmae = $(this).children('span').html();
+        $('.books-fair-btn li a').removeClass('books-fair-btn-active');
+        $('.books-fair-all li a').removeClass('books-fair-list-active');
+        $(this).addClass('books-fair-list-active');
+        $('.book-fair-title').html(BookFairNmae);
+    });
+
+    /*移動至頂端*/
     $('.top').click(function (event) {
-        $('html,body').animate({ scrollTop: 0}, 1000);
+        $('html,body').animate({ scrollTop: 0 }, 1000);
         return false;
-	});
+    });
 
+    /*各區塊頁面連結*/
     $('.menu-list').click(function (event) {
-		//抓出每個連結的距離
         var SrollTop = $($(this).attr('href')).offset().top;
         var MenuHei = $('.menu').height();
-        console.log(MenuHei);
-        $('html,body').animate({ scrollTop: SrollTop - MenuHei}, 1000);
-	});
+        $('html,body').animate({ scrollTop: SrollTop - MenuHei }, 1000);
+    });
 });
